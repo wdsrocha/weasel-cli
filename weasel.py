d379a730679aa9eda0b1d1ec658e4ab7c607aaac
wdsrocha <wesleysr1997@gmail.com>
2019-08-25 01:47:12 -0400

    Add colors to report
    - Move echo to own report function
    - Organize decorators

--- weasel.py
+++ weasel.py
@@ -5,12 +5,31 @@ import click
 
 
 @click.command()
-@click.option('--target', default='METHINKS IT IS LIKE A WEASEL')
-@click.option('--population-size', default=100, type=int)
-@click.option('--mutation-rate', default=.05, type=float)
-def cli(target, population_size, mutation_rate):
+@click.option(
+    '-t', '--target',
+    type=str,
+    default='METHINKS IT IS LIKE A WEASEL',
+    show_default=True)
+@click.option(
+    '-p', '--population-size',
+    type=int,
+    default=100,
+    show_default=True)
+@click.option(
+    '-r', '--mutation-rate',
+    type=float,
+    default=.05,
+    show_default=True)
+@click.option(
+    '--color/--no-color', 'is_colorized',
+    default=True,
+    is_flag=True,
+    show_default=True,
+    help='Tries to use ANSI colors when reporting generation results')
+def cli(target, population_size, mutation_rate, is_colorized):
     charset = string.ascii_uppercase + ' '
     best_subject = ''.join(random.choices(charset, k=len(target)))
+    generation = 0
 
     def mutate(subject):
         return ''.join(
@@ -20,14 +39,20 @@ def cli(target, population_size, mutation_rate):
     def evaluate(subject):
         return sum(a == b for a, b in zip(subject, target))
 
-    generation = 0
+    def report(subject):
+        click.echo(f'{generation: 6}: ', nl=False)
+        styled_subject = ''.join(
+            click.style(a, bold=True, fg='green' if a == b else 'red')
+            for a, b in zip(subject, target))
+        click.echo(styled_subject, color=is_colorized, nl=False)
+        score = evaluate(subject)
+        click.echo(f' -- {score}/{len(target)}')
+
     while True:
         population = population_size * [best_subject]
         population = map(mutate, population)
         best_subject = max(population, key=evaluate)
-        score = evaluate(best_subject)
-        click.echo(
-            f'{generation}: {best_subject} -- score: {score}/{len(target)}')
-        if score == len(target):
+        report(best_subject)
+        if best_subject == target:
             return 0
         generation += 1
